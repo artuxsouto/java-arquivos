5a7b6a3826d4cefb0380a9b1db9eb8fc19670eb0
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-13 11:25:54 -0300

    List files and folder created new dir

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,34 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night"};
+		Scanner sc = new Scanner(System.in);
 		
-		String path = "C:\\temp\\out.txt";
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
 		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}			
-		} catch (IOException e) {
-			e.printStackTrace();			
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\other").mkdir();
+		System.out.println("Directory created sucessfully: " + success);
+		
+		sc.close();
 	}
 }
