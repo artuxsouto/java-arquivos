ffc29b82aa7428428ca9a2dda432aa2785e329dc
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-13 10:35:58 -0300

    Leitura basica de arquivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,29 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		File file = new File("C:\\Temp\\texto.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
 
 	}
-
 }
